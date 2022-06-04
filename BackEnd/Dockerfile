f5a37ab7cb26243356f9fdfcab54629b24ed8e8b
MJSasha <sasha.ushankov@gmail.com>
2022-06-04 19:15:54 +0300

    Hello endpoint

---
+++ BackEnd/Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:18
+EXPOSE 8080
+ADD target/backend.jar backend.jar
+ENTRYPOINT ["java", "-jar", "/backend.jar"]
--- BackEnd/src/main/java/com/example/backend/controllers/HelloController.java
+++ BackEnd/src/main/java/com/example/backend/controllers/HelloController.java
@@ -1,2 +1,14 @@
-package com.example.backend.controllers;public class HelloController {
+package com.example.backend.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity sayHello(){
+        return ResponseEntity.ok("Hello!!");
+    }
 }
