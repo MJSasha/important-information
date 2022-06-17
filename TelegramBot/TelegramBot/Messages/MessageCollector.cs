3944dc5e10c5523de811e56d5ecb7cf79f36c432
MJSasha <sasha.ushankov@gmail.com>
2022-06-17 20:01:32 +0300

    Start bot dev

---
+++ TelegramBot/TelegramBot/Messages/MessageCollector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TelegramBot.Services;
+
+namespace TelegramBot.Messages
+{
+    public class MessageCollector
+    {
+        private readonly BotService bot;
+
+        [Obsolete]
+        public MessageCollector(long chatId)
+        {
+            bot = new BotService(chatId);
+        }
+
+        public Func<Task> StartMenu()
+        {
+            List<List<string>> markup = new()
+            {
+                new List<string>{ "L1B1", "L1B2", "L1B3" },
+                new List<string>{ "L2B1", "L2B2" },
+                new List<string>{ "L3B1" }
+            };
+
+            return () => bot.SendMessage("Стортовое меню", ButtonsGenerater.GetInlineByttons(markup));
+        }
+        public Func<Task> SendText(string text)
+        {
+            return () => bot.SendMessage(text);
+        }
+        public Func<Task> UnknownMessage()
+        {
+            return () => bot.SendMessage("Пока я не понимаю данное сообщение, но скоро научусь");
+        }
+    }
+}
---
+++ TelegramBot/TelegramBot/Services/BotService.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types.ReplyMarkups;
+using TelegramBot.Interfaces;
+
+namespace TelegramBot.Services
+{
+    public class BotService : IBotActions
+    {
+        private readonly long chatId;
+        private readonly TelegramBotClient client = new(AppSettings.Token);
+
+        public BotService(long chatId)
+        {
+            this.chatId = chatId;
+        }
+
+        public async Task SendMessage(string message)
+        {
+            await client.SendTextMessageAsync(chatId, message);
+        }
+
+        public async Task SendMessage(string message, IReplyMarkup buttons)
+        {
+            await client.SendTextMessageAsync(chatId, message, replyMarkup: buttons);
+        }
+    }
+}
