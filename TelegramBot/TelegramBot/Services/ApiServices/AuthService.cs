93c89442d3a57d6154bc794a2dd10da8b2c7182b
mlexanders <mlexanders@outlook.com>
2022-06-22 02:45:32 +0300

    Added registrate in AuthService

--- TelegramBot/TelegramBot/Services/ApiServices/AuthService.cs
+++ TelegramBot/TelegramBot/Services/ApiServices/AuthService.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using TelegramBot.Data.CustomExceptions;
 using TelegramBot.Data.ViewModels;
 using TelegramBot.Interfaces;
 
@@ -9,10 +13,13 @@ namespace TelegramBot.Services.ApiServices
     {
         public AuthService() : base(AppSettings.AuthRoot) { }
 
-        //TODO - реализовать
-        public Task Registrate(RegistrationModel registrationModel, long chatId)
+        public async Task Registrate(RegistrationModel registrationModel, long chatId)
         {
-            throw new NotImplementedException();
+            var json = JsonSerializer.Serialize(registrationModel);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var httpResponse = await httpClient.PostAsync(Root.ToString() + "/" + chatId.ToString(), data);
+            if (!httpResponse.IsSuccessStatusCode) throw new ErrorResponseException(httpResponse.StatusCode,
+                                                                await httpResponse.Content.ReadAsStringAsync());
         }
     }
 }
