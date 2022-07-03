fb176f542aa79da65494c9befb9eef49f3db17da
mlexanders <mlexanders@outlook.com>
2022-07-03 06:36:00 +0300

    adding user interaction(/reg)/beta registration

--- TelegramBot/TelegramBot/Messages/MessageCollector.cs
+++ TelegramBot/TelegramBot/Messages/MessageCollector.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
 using TelegramBot.Interfaces;
 using TelegramBot.Services;
+using TelegramBot.Messages;
+using System.Threading;
 
 namespace TelegramBot.Messages
 {
@@ -18,15 +22,124 @@ namespace TelegramBot.Messages
 
         public Func<Task> StartMenu()
         {
-            List<List<string>> markup = new()
+            List<List<(string, string)>> markup = new()
             {
-                new List<string>{ "L1B1", "L1B2", "L1B3" },
-                new List<string>{ "L2B1", "L2B2" },
-                new List<string>{ "L3B1" }
+                new List<(string, string)>{ ("FirstButton",  "FirstButton")},
+                //new List<(string, string)>{ "L2B1", "L2B2" },
+                //new List<(string, string)>{ "L3B1" }
             };
 
             return () => bot.SendMessage("Стартовое меню", ButtonsGenerater.GetInlineButtons(markup));
         }
+
+
+        private static Registration registrationUser = new Registration();
+        private static CancellationTokenSource ts = new CancellationTokenSource();
+
+        [Obsolete]
+        public async Task RegistrationUsers(long chatId)
+        {
+            try
+            {
+                var client = new TelegramBotClient(AppSettings.Token);
+                client.StartReceiving();
+                client.OnMessage += OnMessageHandlerTwo;
+                await Task.Run(() => IsSuccessRegistration());
+
+            }
+            catch
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+        private static string data;
+
+        private static Task One;
+        private async void IsSuccessRegistration()
+        {
+            try
+            {
+                One = new Task(RegName);
+                await bot.SendMessage("enter name:");
+                One.Wait(ts.Token);
+            }
+            catch (SystemException)
+            {
+                ts.Dispose();
+            }
+            try
+            {
+                ts = new CancellationTokenSource();
+                One = new Task(RegEmail);
+                await bot.SendMessage("enter email:");
+
+                One = new Task(RegEmail);
+                One.Wait(ts.Token);
+            }
+            catch (SystemException)
+            {
+                ts.Dispose();
+            }
+            try
+            {
+                ts = new CancellationTokenSource();
+                await bot.SendMessage("enter password:");
+
+                One = new Task(RegPass);
+                One.Wait(ts.Token);
+            }
+            catch (SystemException)
+            {
+                ts.Dispose();
+            }
+
+            await bot.SendMessage("successfull");
+            await bot.SendMessage(registrationUser.Name);
+            await bot.SendMessage(registrationUser.Email);
+            await bot.SendMessage(registrationUser.Password);
+
+        }
+
+
+        [Obsolete]
+        private static void OnMessageHandlerTwo(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text != "/reg")
+            {
+                Console.WriteLine("OnMessageHandlerTwo: " + e.Message.Text);
+                data = e.Message.Text;
+                if (!ts.IsCancellationRequested) One.Start();
+            }
+        }
+
+        private static void RegName()
+        {
+            if (registrationUser.RegistrationName(data))
+            {
+                ts.Cancel();
+                Console.WriteLine("RegName is succesfull");
+            }
+            else throw new("RegName Bad");
+        }
+        private static void RegEmail()
+        {
+            if (registrationUser.RegistrationEmail(data))
+            {
+                ts.Cancel();
+                Console.WriteLine("RegEmail is succesfull");
+            }
+            else throw new("RegEmail Bad");
+        }
+        private static void RegPass()
+        {
+            if (registrationUser.RegistrationPassword(data))
+            {
+                ts.Cancel();
+                Console.WriteLine("RegPass is succesfull");
+            }
+            else throw new("RegistrationPassword Bad");
+        }
+
         public Func<Task> SendText(string text)
         {
             return () => bot.SendMessage(text);
--- TelegramBot/TelegramBot/Program.cs
+++ TelegramBot/TelegramBot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
@@ -8,15 +9,19 @@ namespace TelegramBot
 {
     internal class Program
     {
+        private static List<long> RegistrationUsersChatId = new();
+        public static TelegramBotClient client = new TelegramBotClient(AppSettings.Token);
+
+
         [Obsolete]
         static void Main(string[] args)
         {
-
             try
             {
-                var client = new TelegramBotClient(AppSettings.Token);
                 client.StartReceiving();
                 client.OnMessage += OnMessageHandler;
+                client.OnMessage += Test;
+                client.OnCallbackQuery += Test;
                 client.OnCallbackQuery += OnCallbackQweryHandlerAsync;
                 Console.ReadLine();
                 client.StopReceiving();
@@ -42,18 +47,47 @@ namespace TelegramBot
         }
 
         [Obsolete]
-        private static async void OnMessageHandler(object sender, MessageEventArgs e)
+        public static async void OnMessageHandler(object sender, MessageEventArgs e)
         {
             MessageCollector message = new(e.Message.Chat.Id);
+            Console.WriteLine(message);
 
             Func<Task> response = e.Message.Text switch
             {
                 "/start" => message.StartMenu(),
+                "/reg" => StartRegistration(e.Message.Chat.Id)
+
+                ,
                 "Привет" => message.SendText("Привет"),
                 _ => message.UnknownMessage()
             };
 
             await response();
         }
+        [Obsolete]
+        public static Func<Task> StartRegistration(long chatId)
+        {
+            RegistrationUsersChatId.Add(chatId);
+            client.StopReceiving();
+            MessageCollector msg = new MessageCollector(chatId);
+            return () => msg.RegistrationUsers(chatId);
+
+        }
+
+
+        [Obsolete]
+        private static void Test(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine(e.Message.Text);
+        }
+
+        [Obsolete]
+        private static void Test(object sender, CallbackQueryEventArgs e)
+        {
+            Console.WriteLine(e.CallbackQuery.Data);
+
+        }
+
+
     }
 }
