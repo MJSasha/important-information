f790155fc4fe53a69c8f24eecfca538761dfc49c
MJSasha <sasha.ushankov@gmail.com>
2022-07-08 23:27:13 +0300

    Minor changes

--- BackEnd/src/main/java/com/example/backend/controllers/UsersController.java
+++ BackEnd/src/main/java/com/example/backend/controllers/UsersController.java
@@ -1,11 +1,9 @@
 package com.example.backend.controllers;
 
-import com.example.backend.baseClasses.BaseCRUDService;
 import com.example.backend.baseClasses.BaseController;
 import com.example.backend.data.exceptions.NotAuthException;
 import com.example.backend.data.models.User;
 import com.example.backend.services.UsersService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
