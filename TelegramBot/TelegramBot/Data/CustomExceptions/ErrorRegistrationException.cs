38075d334473ce738cb8edb34fa67c9bc152b356
mlexanders <mlexanders@outlook.com>
2022-07-15 15:11:07 +0300

    check

---
+++ TelegramBot/TelegramBot/Data/CustomExceptions/ErrorRegistrationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+
+namespace TelegramBot.Data.CustomExceptions
+{
+    public class ErrorRegistrationException : Exception
+    {
+        public string message;
+
+        public ErrorRegistrationException(string message) : base(message)
+        {
+            this.message = message;
+        }
+    }
+}
--- TelegramBot/TelegramBot/Data/ViewModels/RegistrationModel.cs
+++ TelegramBot/TelegramBot/Data/ViewModels/RegistrationModel.cs
@@ -1,7 +1,11 @@
-﻿namespace TelegramBot.Data.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TelegramBot.Data.ViewModels
 {
     public class RegistrationModel
     {
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
