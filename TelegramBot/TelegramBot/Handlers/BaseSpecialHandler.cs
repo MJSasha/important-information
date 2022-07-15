a97c813b04cfbe9d5b61449eeaeb5f91d0e34226
MJSasha <sasha.ushankov@gmail.com>
2022-07-15 13:59:28 +0300

    New reg system and debug

---
+++ TelegramBot/TelegramBot/Handlers/BaseSpecialHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TelegramBot.Interfaces;
+
+namespace TelegramBot.Handlers
+{
+    public abstract class BaseSpecialHandler
+    {
+        protected Task currentTask;
+        protected CancellationTokenSource сancellationToken;
+        protected readonly IBotService bot;
+
+        protected BaseSpecialHandler(IBotService bot)
+        {
+            this.bot = bot;
+        }
+
+        [Obsolete]
+        public abstract Task ProcessMessage(string registrationMassage);
+
+        protected void AddProcessing(string message, Action action, Action completeAction = null)
+        {
+            сancellationToken = new();
+            currentTask = new Task(() =>
+            {
+                action();
+                сancellationToken.Cancel();
+            });
+            bot.SendMessage(message);
+            currentTask.Wait();
+
+            completeAction?.Invoke();
+        }
+    }
+}
--- TelegramBot/TelegramBot/Interfaces/IHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace TelegramBot.Interfaces
-{
-    public interface IHandler
-    {
-        public Task ProcessMessage(string message);
-    }
-}
