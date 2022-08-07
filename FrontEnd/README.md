8650549984ff2c40b7fa27bdcfbfc7d9abb6fffe
Danila <samsonchik2406@mail.ru>
2022-08-07 19:32:29 +0300

    Bug fixes & adding datepicker

--- FrontEnd/README.md
+++ FrontEnd/README.md
@@ -4,7 +4,7 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ## Available Scripts
 
-In the project directory, you can run:
+In the project directory(in terminal use this command `cd FrontEnd/src/main`), you can run:
 
 ### `npm start`
 
--- FrontEnd/src/main/src/App/MainPage/Main/Main.js
+++ FrontEnd/src/main/src/App/MainPage/Main/Main.js
@@ -2,9 +2,10 @@ import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import {CSSTransition} from 'react-transition-group';
-import SideBar from './SideBar/SideBar';
+import SideBar from '../SideBar/SideBar';
 import './Main.modules.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDatePicker from '../DatePicker/DatePicker'
 
 
 
@@ -25,13 +26,20 @@ function Main(){
                 <header className="header d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
                     <h1 className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">LOGO</h1>
                     <div className="col-md-3 group-btn">
-                        <button type="button" className="btn btn-outline-primary me-2" onClick={()=>setShowSB(!showSB)}>{showSB ? 'X' : 'Новости'}</button>
+                        <button type="button" className="btn btn-outline-primary me-2 news-btn" onClick={()=>setShowSB(!showSB)}>Новости</button>
                         <button onClick={UserLogout} className="logout-btn btn btn-primary">Logout</button>
                     </div>
                 </header>
             </div>
             <div className="content-wrapper">
-                <div className="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quaerat consequatur atque architecto illo debitis dolorem, molestias nulla porro dolor veniam totam quisquam? Sapiente odio maiores, culpa corrupti numquam dolorem?</div>
+                {/* date */}
+                <div className="container">
+                        <ReactDatePicker />
+
+                        <div className="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quaerat consequatur atque architecto illo debitis dolorem, molestias nulla porro dolor veniam totam quisquam? Sapiente odio maiores, culpa corrupti numquam dolorem?</div>
+                </div>
+                {/* date */}
+                
                 <CSSTransition in={showSB} timeout={300} classNames='fade' unmountOnExit>
                 <div className="SideBarWrapper">
                     <SideBar/>
