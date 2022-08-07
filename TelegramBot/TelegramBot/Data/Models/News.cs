6c48621fcd375d7e4ca450ddf0d15e7b3d29503b
MJSasha <sasha.ushankov@gmail.com>
2022-08-07 22:07:36 +0300

    update news

--- TelegramBot/TelegramBot/Data/Models/News.cs
+++ TelegramBot/TelegramBot/Data/Models/News.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Linq;
+using System.Text.Json.Serialization;
 
 namespace TelegramBot.Data.Models
 {
@@ -6,9 +7,19 @@ namespace TelegramBot.Data.Models
     {
         [JsonPropertyName("id")]
         public int Id { get; set; }
+
         [JsonPropertyName("message")]
         public string Message { get; set; }
+
         [JsonPropertyName("needToSend")]
         public bool NeedToSend { get; set; }
+
+        [JsonPropertyName("pictures")]
+        private string pictures;
+
+        public string[] GetPictures => pictures.Split("|");
+        public void CleanPictures() => pictures = "";
+        public void AddPictures(string[] pictures) => pictures.ToList().ForEach(picture => this.pictures += $"{picture}|");
+        public void AddPicture(string picture) => pictures += $"{picture}|";
     }
 }
