9163697681f3032e90f92f79b715c271d976cc04
MJSasha <sasha.ushankov@gmail.com>
2022-08-20 09:48:10 +0300

    Update models - Add lessons link to news

--- BackEnd/src/main/resources/data.sql
+++ BackEnd/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO lessons (id, information, name, teacher)
+INSERT INTO lessons (id, name, information, teacher)
 VALUES (1, 'Lesson (1)', 'Info (1)', 'Teacher (1)'),
        (2, 'Lesson (2)', 'Info (2)', 'Teacher (2)'),
        (3, 'Lesson (3)', 'Info (3)', 'Teacher (3)'),
@@ -34,28 +34,28 @@ VALUES (1, 1),
        (3, 5),
        (3, 6);
 
-INSERT INTO news (id, date_time_of_create, message, need_to_send, pictures)
-VALUES (1, NOW(), 'Is sent message (1)', 0, 'test|'),
-       (2, DATE_SUB(NOW(), INTERVAL 1 DAY), 'Not sent message (2)', 1, 'test|test|'),
-       (3, DATE_SUB(NOW(), INTERVAL 2 DAY), 'Is sent message (3)', 0, 'test|test|test|'),
-       (4, DATE_SUB(NOW(), INTERVAL 3 DAY), 'Not sent message (4)', 1, ''),
-       (5, DATE_SUB(NOW(), INTERVAL 4 DAY), 'Is sent message (5)', 0, ''),
-       (6, DATE_SUB(NOW(), INTERVAL 5 DAY), 'Not sent message (6)', 1, ''),
-       (7, DATE_SUB(NOW(), INTERVAL 6 DAY), 'Is sent message (7)', 0, ''),
-       (8, DATE_SUB(NOW(), INTERVAL 7 DAY), 'Not sent message (8)', 1, ''),
-       (9, DATE_SUB(NOW(), INTERVAL 8 DAY), 'Is sent message (9)', 0, ''),
-       (10, DATE_SUB(NOW(), INTERVAL 9 DAY), 'Not sent message (10)', 1, ''),
-       (11, DATE_SUB(NOW(), INTERVAL 10 DAY), 'Is sent message (11)', 0, ''),
-       (12, DATE_SUB(NOW(), INTERVAL 11 DAY), 'Not sent message (12)', 1, ''),
-       (13, DATE_SUB(NOW(), INTERVAL 12 DAY), 'Is sent message (13)', 0, ''),
-       (14, DATE_SUB(NOW(), INTERVAL 13 DAY), 'Not sent message (14)', 1, ''),
-       (15, DATE_SUB(NOW(), INTERVAL 14 DAY), 'Is sent message (15)', 0, ''),
-       (16, DATE_SUB(NOW(), INTERVAL 15 DAY), 'Not sent message (16)', 1, ''),
-       (17, DATE_SUB(NOW(), INTERVAL 16 DAY), 'Is sent message (17)', 0, ''),
-       (18, DATE_SUB(NOW(), INTERVAL 17 DAY), 'Not sent message (18)', 1, ''),
-       (19, DATE_SUB(NOW(), INTERVAL 18 DAY), 'Is sent message (19)', 0, ''),
-       (20, DATE_SUB(NOW(), INTERVAL 19 DAY), 'Not sent message (20)', 1, ''),
-       (21, DATE_SUB(NOW(), INTERVAL 20 DAY), 'Is sent message (21)', 0, '');
+INSERT INTO news (id, date_time_of_create, message, need_to_send, pictures, lesson_id)
+VALUES (1, NOW(), 'Is sent message (1)', 0, 'test|', NULL),
+       (2, DATE_SUB(NOW(), INTERVAL 1 DAY), 'Not sent message (2)', 1, 'test|test|', 1),
+       (3, DATE_SUB(NOW(), INTERVAL 2 DAY), 'Is sent message (3)', 0, 'test|test|test|', NULL),
+       (4, DATE_SUB(NOW(), INTERVAL 3 DAY), 'Not sent message (4)', 1, '', 2),
+       (5, DATE_SUB(NOW(), INTERVAL 4 DAY), 'Is sent message (5)', 0, '', NULL),
+       (6, DATE_SUB(NOW(), INTERVAL 5 DAY), 'Not sent message (6)', 1, '', 3),
+       (7, DATE_SUB(NOW(), INTERVAL 6 DAY), 'Is sent message (7)', 0, '', NULL),
+       (8, DATE_SUB(NOW(), INTERVAL 7 DAY), 'Not sent message (8)', 1, '', 4),
+       (9, DATE_SUB(NOW(), INTERVAL 8 DAY), 'Is sent message (9)', 0, '', NULL),
+       (10, DATE_SUB(NOW(), INTERVAL 9 DAY), 'Not sent message (10)', 1, '', 5),
+       (11, DATE_SUB(NOW(), INTERVAL 10 DAY), 'Is sent message (11)', 0, '', NULL),
+       (12, DATE_SUB(NOW(), INTERVAL 11 DAY), 'Not sent message (12)', 1, '', 6),
+       (13, DATE_SUB(NOW(), INTERVAL 12 DAY), 'Is sent message (13)', 0, '', NULL),
+       (14, DATE_SUB(NOW(), INTERVAL 13 DAY), 'Not sent message (14)', 1, '', 7),
+       (15, DATE_SUB(NOW(), INTERVAL 14 DAY), 'Is sent message (15)', 0, '', NULL),
+       (16, DATE_SUB(NOW(), INTERVAL 15 DAY), 'Not sent message (16)', 1, '', 8),
+       (17, DATE_SUB(NOW(), INTERVAL 16 DAY), 'Is sent message (17)', 0, '', NULL),
+       (18, DATE_SUB(NOW(), INTERVAL 17 DAY), 'Not sent message (18)', 1, '', 9),
+       (19, DATE_SUB(NOW(), INTERVAL 18 DAY), 'Is sent message (19)', 0, '', NULL),
+       (20, DATE_SUB(NOW(), INTERVAL 19 DAY), 'Not sent message (20)', 1, '', 10),
+       (21, DATE_SUB(NOW(), INTERVAL 20 DAY), 'Is sent message (21)', 0, '', NULL);
 
 INSERT INTO passwords (id, value)
 VALUES (1, 'admin');
