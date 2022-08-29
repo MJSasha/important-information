fe2bdb77cc778369ee7ec1e61b34486547114ef8
MJSasha <sasha.ushankov@gmail.com>
2022-08-29 19:19:52 +0300

    Env name fix

--- TelegramBot/TelegramBot/AppSettings.cs
+++ TelegramBot/TelegramBot/AppSettings.cs
@@ -5,7 +5,7 @@ namespace TelegramBot
     public static class AppSettings
     {
         public static string BotToken { get; } = Environment.GetEnvironmentVariable("API_TOKEN") ?? "2065215367:AAHxs51AowRJAqefe3tvV7d5jn5nsC_-xDc";
-        public static string ApiToken { get; } = Environment.GetEnvironmentVariable("API_TOKEN") ?? "Fp9u5dsvcdM3XIm";
+        public static string ApiToken { get; } = Environment.GetEnvironmentVariable("BOT_TOKEN") ?? "Fp9u5dsvcdM3XIm";
         public static string BackRoot { get; } = Environment.GetEnvironmentVariable("BACK_ROOT") ?? "http://localhost:8080/api/";
         public static string FrontRoot { get; } = Environment.GetEnvironmentVariable("FRONT_ROOT") ?? "google.com";
 
