34f138d0f3e9d21e64484307d3907b1d6733f0e7
mlexanders <mlexanders@outlook.com>
2022-11-03 23:02:38 +0300

    ~

--- Infrastructure/ImpInfCommon/Interfaces/IBaseCRUDService.cs
+++ Infrastructure/ImpInfCommon/Interfaces/IBaseCRUDService.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
 
 namespace ImpInfCommon.Interfaces
 {
@@ -8,9 +6,9 @@ namespace ImpInfCommon.Interfaces
     {
         Task<TEntity[]> Get();
         Task<TEntity> Get(int id);
-        Task<ObjectResult> Post(TEntity entity);
-        Task<ObjectResult> Post(TEntity[] entities);
-        Task<ObjectResult> Patch(int id, TEntity entity);
-        Task<ObjectResult> Delete(int id);
+        //Task<ObjectResult> Post(TEntity entity);
+        //Task<ObjectResult> Post(TEntity[] entities);
+        //Task<ObjectResult> Patch(int id, TEntity entity);
+        //Task<ObjectResult> Delete(int id);
     }
 }
