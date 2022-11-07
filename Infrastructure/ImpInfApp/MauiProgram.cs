b65eb2041f350bf5cbf84d4c9517395549f60da6
mlexanders <mlexanders@outlook.com>
2022-11-07 21:24:44 +0300

    change DI in maui App

--- Infrastructure/ImpInfApp/MauiProgram.cs
+++ Infrastructure/ImpInfApp/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using ImpInfCommon.ApiServices;
+using ImpInfCommon.Interfaces;
 using ImpInfFrontCommon.Services;
 using ImpInfFrontCommon.Utils;
 using Microsoft.AspNetCore.Components.Authorization;
@@ -32,12 +33,11 @@ namespace ImpInfApp
                 .AddHttpClient("API", client => client.BaseAddress = new Uri(backRoot)).AddHttpMessageHandler<CookieHandler>();
 
             builder.Services.AddTransient(sp => new AuthService(backRoot, sp.GetService<HttpClient>(), "Account"))
-                            .AddTransient(sp => new DaysServices(backRoot, sp.GetService<HttpClient>()))
-                            .AddTransient(sp => new LessonsService(backRoot, sp.GetService<HttpClient>()))
-                            .AddTransient(sp => new NewsService(backRoot, sp.GetService<HttpClient>()))
-                            .AddTransient(sp => new NotesService(backRoot, sp.GetService<HttpClient>()))
-                            .AddTransient(sp => new UsersService(backRoot, sp.GetService<HttpClient>()))
-                            .AddTransient(sp => new UsersService(backRoot, sp.GetService<HttpClient>()));
+                .AddTransient<IDays, DaysServices>(sp => new DaysServices(backRoot, sp.GetService<HttpClient>()))
+                .AddTransient<ILesson, LessonsService>(sp => new LessonsService(backRoot, sp.GetService<HttpClient>()))
+                .AddTransient<INews, NewsService>(sp => new NewsService(backRoot, sp.GetService<HttpClient>()))
+                .AddTransient<INotes, NotesService>(sp => new NotesService(backRoot, sp.GetService<HttpClient>()))
+                .AddTransient<IUser, UsersService>(sp => new UsersService(backRoot, sp.GetService<HttpClient>()));
 
             builder.Services.AddOptions();
             builder.Services.AddAuthorizationCore();
