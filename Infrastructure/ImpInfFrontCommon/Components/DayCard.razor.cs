be21ea3a81558ba37db84a0e38ad05e5a56761f4
MJSasha <sasha.ushankov@gmail.com>
2022-12-30 22:26:01 +0300

    Add day card expander

---
+++ Infrastructure/ImpInfFrontCommon/Components/DayCard.razor.cs
@@ -0,0 +1,25 @@
+﻿using ImpInfCommon.Data.Definitions;
+using ImpInfCommon.Data.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace ImpInfFrontCommon.Components
+{
+    public partial class DayCard : ComponentBase
+    {
+        [Parameter]
+        public Day Day { get; set; }
+
+        private List<Note> notes;
+
+        private string GetImporatanceColor(Importance importance)
+        {
+            return importance switch
+            {
+                Importance.VeryImportant => "--veryimportant-color",
+                Importance.Important => "--important-color",
+                Importance.NotImportant => "--notimportant-color",
+                _ => ""
+            };
+        }
+    }
+}
